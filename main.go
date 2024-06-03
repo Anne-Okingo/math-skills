bf743049fca1742eeab8dba1685ec758454878f5
aokingo <annekadiso@gmail.com>
2024-06-03 17:01:26 +0300

    Updating main file

--- main.go
+++ main.go
@@ -1,92 +1,30 @@
-// package main
-
-// import (
-// 	"fmt"
-// 	"os"
-// 	"strconv"
-// 	"strings"
-
-// 	//"golang.org/x/text/number"
-// 	functions "math-skills/function"
-// )
-
-// func main() {
-// 	// fmt.Println(functions.Average([]int{1, 2, 3, 4}))
-// 	args := os.Args
-
-// 	if len(args) != 2 {
-// 		fmt.Println("usage: go run <main.go><data.txt>")
-// 		os.Exit(0)
-// 	}
-// 	filedata := os.Args[1]
-
-// 	fileinfo, _ := os.Stat(filedata)
-// 	if fileinfo.Size() == 0 {
-// 		fmt.Println("Empty file")
-// 		os.Exit(1)
-// 	}
-
-// 	file, err := os.ReadFile(filedata)
-// 	if err != nil {
-// 		fmt.Println("error reading file", err)
-// 	}
-// 	var values []float64
-// 	filecontent := strings.Split(string(file), "\n")
-// 	// if filecontent == nil {
-// 	// 	return
-// 	// }
-// 	for _, num := range filecontent {
-// 		if  num == "" {
-// 			continue
-// 		}
-// 		 number, err := strconv.ParseFloat(num, 64)
-// 		if err != nil {
-// 			fmt.Println("error in conversion", err)
-// 			return
-// 		}
-// 		values = append(values, number)
-// 	}
-
-// 	output := functions.HandleAverage(values)
-// 	output1 := functions.Median(values)
-// 	output2 := functions.HandleVariance(values)
-// 	output3 := functions.StandardDev(values)
-
-//		fmt.Printf("Average: %.0f\n ", output)
-//		fmt.Printf("Median: %.0f\n", output1)
-//		fmt.Printf("Variance: %.0f\n", output2)
-//		fmt.Printf("Standard Deviation: %.0f\n ", output3)
-//	}
 package main
 
 import (
 	"fmt"
+	"mathskills/function"
 	"os"
 	"strconv"
 	"strings"
-	"mathskills/function"
 )
 
 func main() {
 	args := os.Args
 
+	// A condition to ensure the program only takes 2 arguments
 	if len(args) != 2 {
 		fmt.Println("usage : go run . <datafile.txt>")
+		return
 	}
 
 	filename := args[1]
-
-	// filestatus, _ := os.Stat(filename)
-	// if filestatus.Size() == 0 {
-	// 	fmt.Println("Error : empty file")
-	// 	os.Exit(1)
-	// }
 
 	filecontent, err := os.ReadFile(filename)
 	if err != nil {
 		fmt.Println("Error reading file", err)
 	}
-	if len(filecontent) == 0{
+	// checking if the file containing data is empty
+	if len(filecontent) == 0 {
 		fmt.Println("empty file")
 		return
 	}
@@ -94,10 +32,13 @@ func main() {
 	contentsplit := strings.Split(string(filecontent), "\n")
 
 	values := []float64{}
+	// checking if there are empty spaces/ unfilled data among the data set, if there is they are ignored
 	for _, ch := range contentsplit {
+		ch = strings.TrimSpace(ch)
 		if ch == "" {
 			continue
 		}
+		// converting each character(data) into float64
 		dataset, err := strconv.ParseFloat(ch, 64)
 		if err != nil {
 			fmt.Println("Error in conversion", err)
@@ -111,8 +52,8 @@ func main() {
 	result3 := functions.Variance(values)
 	result4 := functions.StandardDev(values)
 
-	fmt.Printf("Average : %.0f\n",result1)
-	fmt.Printf("Median : %.0f\n",result2)
-	fmt.Printf("Variance : %.0f\n",result3)
-	fmt.Printf("Standard Deviation : %.0f\n",result4)
+	fmt.Printf("Average : %.0f\n", result1)
+	fmt.Printf("Median : %.0f\n", result2)
+	fmt.Printf("Variance : %.0f\n", result3)
+	fmt.Printf("Standard Deviation : %.0f\n", result4)
 }
