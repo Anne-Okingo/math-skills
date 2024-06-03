53694d643538293f1cfd02b6dec9234256373caf
aokingo <annekadiso@gmail.com>
2024-06-03 17:12:25 +0300

    updating variance fuction

--- function/variance.go
+++ function/variance.go
@@ -1,15 +1,13 @@
 package functions
 
-//import 
-
 func Variance(number []float64) float64 {
 	sum := 0.0
 
 	mean := Average(number)
 
 	for _, char := range number {
-		sum += (char -mean)*(char-mean)
+		sum += (char - mean) * (char - mean)
 	}
-	return sum/float64(len(number))
+	return sum / float64(len(number))
 
 }
