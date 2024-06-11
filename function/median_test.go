0c35c7482ca1a247886c530935e65c021129235e
aokingo <annekadiso@gmail.com>
2024-06-11 11:47:10 +0300

    fix : fixing a bug

--- function/median_test.go
+++ function/median_test.go
@@ -9,9 +9,9 @@ import (
 func TestMedian(t *testing.T) {
 	input := []float64{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	expected := float64(5)
-	result := Median(input)
+	results := Median(input)
 
-	if !reflect.DeepEqual((math.Round(result)), expected) {
-		t.Errorf("Test Medial Failed Expected : %v. Got : %v .", expected, result)
+	if !reflect.DeepEqual((math.Round(results)), expected) {
+		t.Errorf("Test Medial Failed Expected : %v. Got : %v .", expected, results)
 	}
 }
