89f9ccb70a4c280f53fbc44ffc02410700fad808
aokingo <annekadiso@gmail.com>
2024-06-11 11:45:38 +0300

    fix : fixing a bug

--- function/standarddev_test.go
+++ function/standarddev_test.go
@@ -10,9 +10,9 @@ func TestStandardDev(t *testing.T) {
 	input := []float64{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	expected := float64(3)
 
-	result := StandardDev(input)
+	results := StandardDev(input)
 
-	if !reflect.DeepEqual(math.Round(result), expected) {
-		t.Errorf("Test StandardDev Failed Expected : %v, Got : %v ", expected, result)
+	if !reflect.DeepEqual(math.Round(results), expected) {
+		t.Errorf("Test StandardDev Failed Expected : %v, Got : %v ", expected, results)
 	}
 }
