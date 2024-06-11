534e2cae134ac62b637996a44ad5a413a1682a0b
aokingo <annekadiso@gmail.com>
2024-06-11 11:42:15 +0300

    fix : fixing a bug

--- function/variance_test.go
+++ function/variance_test.go
@@ -10,9 +10,9 @@ func TestVariance(t *testing.T) {
 	input := []float64{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	expected := float64(7)
 
-	result := Variance(input)
+	results := Variance(input)
 
-	if !reflect.DeepEqual(math.Round(result), expected) {
-		t.Errorf("Test Variance failed Expected : %v Got : %v", expected, result)
+	if !reflect.DeepEqual(math.Round(results), expected) {
+		t.Errorf("Test Variance failed Expected : %v Got : %v", expected, results)
 	}
 }
